d44f0af8d66d17b022abd8a69cde058c7e527c7c
mauricekollewe <kollewem.35@gmail.com>
2022-02-07 21:53:50 +0000

    add background-image and updates

---
+++ clothes-app/src/component/common/Header.js
@@ -0,0 +1,29 @@
+// import React from 'react'
+// // import { useSearchParams } from 'react-router-dom'
+// // import { Text, View } from 'react-native'
+// // import { renderMatches } from 'react-router-dom'
+// import SearchClothe from './Search'
+
+// export default class Header extends Component {
+// 	constructor(props) {
+// 		super(props)
+// 		this.state = { whatever: '' }
+// 		this.onSearch = this.onSearch.bind(this)
+// 	}
+
+// 	onSearch(value) {
+// 		if(props === title) {
+// 			return true
+// 		} else {
+// 			return 'No Matches'
+// 		}
+// 	}
+
+// 	render() {
+// 		render(
+// 			<div>
+// 				<SearchClothe onSubmit={this.onSearch}>
+// 			</div>
+// 		)
+// 	}
+// }
--- clothes-app/src/component/common/Navigation.js
+++ clothes-app/src/component/common/Navigation.js
@@ -1,12 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap'
 import { MDBCol } from 'mdbreact'
-import SearchClothe from './SearchClothe'
+import axios from 'axios'
+import { Button } from 'react-bootstrap'
+
 
 
 
 const Navigation = () => {
+	const [search, setSearch] = useState([])
+	const [filteredData, setFilteredData] = useState(search)
 	
+
+	useEffect(() => {
+		const getData = async () => {
+			try {
+				const { data } = await axios.get('https://5fd9d76f6cf2e7001737ead3.mockapi.io/api/v1/dress')
+				setSearch(data)
+				filteredData(data)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		getData()
+	}, [])
+
+
+	const handleSearch = (event) => {
+		let value = event.target.value.toUpperCase()
+		let result = []
+		console.log(value)
+		result = search.filter((data) => {
+			return data.title.search(value) !== -1
+		})
+		setFilteredData(result)
+		console.log(result)
+	}
+
+
+	const handleSubmit = (event) => {
+  event.preventDefault()
+		// console.log(e.target.element.title.value)
+    console.log(event.target.title.value)
+		if('result' === 'title') {
+			return true
+		} else {
+			return "No Matches"
+		}
+  }
+
 
 	return (
 		<Navbar expand="lg">
@@ -14,8 +56,24 @@ const Navigation = () => {
 				<Container fluid>
 					<Navbar.Brand href="/Home">👗</Navbar.Brand>
 					<Nav.Link href="/clothes">Clothes</Nav.Link>
-					<MDBCol md="4">
-						<SearchClothe />
+					<MDBCol md="6">
+						<div className="input-group md-form form-sm form-1 pl-0" {...filteredData.map((value, index) => {
+							return (
+								<div>
+									<div key={value.id}>
+									</div>
+								</div>
+							)
+						})}>
+							<div className="input-group-prepend">
+							</div>
+							<input className="form-control my-0 py-1" type="text" onChange={(event) =>
+								handleSearch(event)}
+								placeholder="Search" arial-label="Search" />
+						</div>
+						<div>
+							<Button onSubmit={handleSubmit} variant="outline-success">search</Button>
+						</div>
 					</MDBCol>
 					<Nav.Link href="/Home">Home</Nav.Link>
 					<Navbar.Brand href="/Home">👗</Navbar.Brand>
